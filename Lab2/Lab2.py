d128c930cfa321e59d490b398856958b3fb1ff08
Aditya-Bani <bani96963@gmail.com>
2020-11-03 17:08:35 +0700

    Upload Gambar

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -2,7 +2,7 @@ a=input("masukkan nilai a:")
 b=input("masukkan nilai b:")
 print("variable a=",a)
 print("variable b=",b)
-print("hasil penggabungan {1}&{0}=%a".format(a,b) %(a+b))
+print("hasil penggabungan {1}&{0}=%d".format(a,b) %(a+b))
 
 #konversi nilai variable
 a=int(a)
